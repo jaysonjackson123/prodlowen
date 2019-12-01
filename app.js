f1844b88955f3f1c5af7b08ccb1276de66d155d4
Jayson Jackson <jayjacksonjay@hotmail.com>
2019-12-01 14:33:29 -0500

    0.15 home screen init

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.get("/",function(req,res){
 
 
 
-
-
-app.listen(process.env.PORT || 5000)
+var server = app.listen(8080,function() {
+   var host = server.address().address
+   var port = server.address().port  
+   console.log("Example app listening at http://%s:%s", host, port)
+});
