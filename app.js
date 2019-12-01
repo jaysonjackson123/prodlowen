6fb17986d8202b32f1b7bf4ede8b698282f44489
Jayson Jackson <jayjacksonjay@hotmail.com>
2019-11-30 20:35:27 -0500

    0.14.1 stable css link

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ app         		  = express();
 
 app.set('view engine','ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/",function(req,res){
 	res.render("home");
 });
 
 // This function is for Heroku purposes
-app.listen(process.env.PORT || 5000)
-
-
-
+var server = app.listen(8080,function() {
+   var host = server.address().address
+   var port = server.address().port  
+   console.log("Example app listening at http://%s:%s", host, port)
+});
 
