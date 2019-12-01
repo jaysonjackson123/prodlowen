3431f115502f0744f702a3344cd1f1f79b3080a8
Jayson Jackson <jayjacksonjay@hotmail.com>
2019-12-01 12:42:12 -0500

    0.14.51 server listen hotfix

--- app.js
+++ app.js
@@ -25,9 +25,5 @@ app.get("/",function(req,res){
 
 
 
-var server = app.listen(8080,function() {
-   var host = server.address().address
-   var port = server.address().port  
-   console.log("Example app listening at http://%s:%s", host, port)
-});
+app.listen(process.env.PORT || 5000)
 
