1a4ac12513b3faa3a7a12a6dd874043898b4435f
Jayson Jackson <jayjacksonjay@hotmail.com>
2019-12-02 08:46:51 -0500

    0.15.3 songSchema created / basic input form

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 var express 		  = require("express"),
 mongoose   			  = require("mongoose"),
 express     		  = require("express"),
-app         		  = express();
+passport              = require("passport");
+bodyParser  		  = require("body-parser"),
+app         		  = express(),
+LocalStrategy         = require("passport-local"),
+passportLocalMongoose = require("passport-local-mongoose");
 
+
+// Sets what file type is default
 app.set('view engine','ejs');
 
+// Sets what folder to look for things in
 app.use(express.static(__dirname + '/public'));
 
-
-
-
-
+// Connecting to database locally
+mongoose.connect("mongodb://localhost/prodlowen");
 
 // Main Screen
 app.get("/",function(req,res){
 	res.render("home");
 });
 
+// Creating a new song
 
 
 
-
-
-
-
-app.listen(process.env.PORT || 5000)
+// Server listen port
+var server = app.listen(8080,function() {
+   var host = server.address().address
+   var port = server.address().port  
+   console.log("Example app listening at http://%s:%s", host, port)
+});
---
+++ models/songSchema.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var songSchema = new mongoose.Schema({
+	username: String,
+	password: String,
+	song-url: String,
+	art-url: String
+});
+
+
+
+//Exports a model of the mongoose schema SongSchema, passed as Song
+module.exports = mongoose.model("Song",SongSchema);
+
+//
+
